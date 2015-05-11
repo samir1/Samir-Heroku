574853f7e668d61388b5ef3d7826179a352b7828
Samir <samir1@brandeis.edu>
2015-05-11 02:51:32 -0400

    initial commit

---
+++ app.rb
@@ -0,0 +1,39 @@
+require 'sinatra'
+require 'forecast_io'
+
+configure do
+	ForecastIO.api_key = ENV['FORECASTIO_KEY']
+	@@lat = 29.779312
+	@@lng = -95.460570
+end
+
+get '/' do
+	erb :main
+end
+
+get '/apparentTemperature' do
+	"#{ForecastIO.forecast(@@lat, @@lng)[:currently][:apparentTemperature]}"
+end
+
+# get 'precipProbability' do
+# 	"#{ForecastIO.forecast(@@lat, @@lng)[:currently][:precipProbability]}"
+# end
+
+get '/precipProbability' do
+  "#{ForecastIO.forecast(@@lat, @@lng)[:currently][:precipProbability]}"
+end
+
+get '/summary' do
+	"#{ForecastIO.forecast(@@lat, @@lng)[:currently][:summary]}"
+end
+
+get '/updatelatlng/:lat/:lng' do
+	puts 'updating lat lng'
+	puts "old lat is #{@@lat}"
+	puts "old lng is #{@@lng}"
+	@@lat = params[:lat]
+	@@lng = params[:lng]
+	puts "new lat is #{@@lat}"
+	puts "new lng is #{@@lng}"
+	redirect to '/'
+end
